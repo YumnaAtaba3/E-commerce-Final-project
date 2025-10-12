a94af42b11e428f82477fa9add4bb6afa46c2fbc
YumnaAtaba3 <yumnaat33@gmail.com>
2025-10-12 13:08:11 +0300

    "deploy"
    Signed-off-by: YumnaAtaba3 <yumnaat33@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/e-commerce-final-project/',
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 2000 // optional, increases warning threshold
+  }
+});
+
