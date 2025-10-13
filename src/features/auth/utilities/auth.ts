455dd7d6f69896d0fbc0c25c6d2686ae3848d2c0
YumnaAtaba3 <yumnaat33@gmail.com>
2025-10-14 02:07:54 +0300

    "try vercel to deploy"
    Signed-off-by: YumnaAtaba3 <yumnaat33@gmail.com>

--- src/features/auth/utilities/auth.ts
+++ src/features/auth/utilities/auth.ts
@@ -15,3 +15,25 @@ export function logoutHelper(fallbackUrl = appRoutes.auth.signUp) {
     window.location.href = `${fallbackUrl.replace(/^\//, "")}`;
   }
 }
+
+// import { appRoutes } from "../../../routes";
+// import { userStorage } from "../storage/userStorage";
+
+// export function logoutHelper(fallbackUrl = appRoutes.auth.signUp) {
+//   try {
+//     userStorage.remove();
+//     localStorage.removeItem("refreshToken");
+//     localStorage.removeItem("user");
+//   } catch (error) {
+//     console.error("Logout cleanup failed:", error);
+//   }
+
+//   if (typeof window !== "undefined") {
+//     const base =
+//       window.location.hostname.includes("github.io")
+//         ? "/E-commerce-Final-project/"
+//         : import.meta.env.BASE_URL || "/";
+
+//     window.location.href = `${base}${fallbackUrl.replace(/^\//, "")}`;
+//   }
+// }
